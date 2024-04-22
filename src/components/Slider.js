0599a235059db8d381affb9cafc10c6f5a389d69
bishwarup307 <15094759+bishwarup307@users.noreply.github.com>
2024-04-22 18:39:59 +0530

    Add dot navigations

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -15,6 +15,9 @@ let inFocusLeft;
 let right = 3;
 let inFocusRight;
 
+let activeNav;
+const navButtons = [];
+
 const getAdjacentImage = function getNext(index, direction) {
     const img = document.createElement("img");
     img.src = IMAGE_URLS[index];
@@ -37,12 +40,35 @@ const getCenterIndex = function getCenterSlideIndex(prevCenter, direction) {
     return centerIndex;
 };
 
+const NaVButton = function makeNavButton(index) {
+    const btnNav = document.createElement("button");
+    btnNav.className = "nav-dot w-3 h-3 bg-gray-400 rounded-full";
+    if (index === 2) btnNav.classList.add("active");
+    btnNav.dataset.index = index;
+
+    function ButtonElement() {
+        return btnNav;
+    }
+
+    function activate() {
+        btnNav.classList.add("active");
+        activeNav = this;
+    }
+
+    function deactivate() {
+        btnNav.classList.remove("active");
+    }
+
+    return { ButtonElement, activate, deactivate };
+};
+
 export default function Slider() {
     const parentContainer = document.createElement("div");
+    parentContainer.className = "relative";
 
     const container = document.createElement("div");
     container.className =
-        "container relative py-5 text-white mx-auto flex justify-center items-center gap-8 overflow-hidden";
+        "container relative py-5 text-white mx-auto flex justify-center items-center gap-8 overflow-hidden xl:max-w-7xl";
 
     const leftDiv = document.createElement("div");
     leftDiv.className =
@@ -63,16 +89,17 @@ export default function Slider() {
         "absolute w-full h-full rounded-xl transition-transform ease-in-expo duration-1000";
     focusDiv.appendChild(focusImgM);
 
+    const className =
+        "absolute z-50 top-1/2 px-1 py-1 bg-white bg-opacity-50 fill-slate-600 w-8 h-8 -translate-y-1/2 rounded-full hover:bg-slate-700 hover:fill-slate-50 md:w-10 md:h-10 lg:w-14 lg:h-14";
     const leftButton = document.createElement("button");
     leftButton.innerHTML = `<svg viewBox="-8.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>left</title> <path d="M7.094 15.938l7.688 7.688-3.719 3.563-11.063-11.063 11.313-11.344 3.531 3.5z"></path> </g></svg>`;
-    leftButton.className =
-        "absolute z-50 top-1/2 left-4 px-2 py-2 bg-white bg-opacity-50 fill-slate-600 w-14 h-14 -translate-y-1/2 rounded-full hover:bg-slate-700 hover:fill-slate-50";
+    leftButton.className = className;
+    leftButton.classList.add("left-4");
     focusDiv.appendChild(leftButton);
-
     const rightButton = document.createElement("button");
     rightButton.innerHTML = `<svg viewBox="-8.5 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <title>right</title> <path d="M7.75 16.063l-7.688-7.688 3.719-3.594 11.063 11.094-11.344 11.313-3.5-3.469z"></path> </g></svg>`;
-    rightButton.className =
-        "absolute z-50 top-1/2 right-4 px-2 py-2 bg-white bg-opacity-50 fill-slate-600 w-14 h-14 -translate-y-1/2 rounded-full hover:bg-slate-700 hover:fill-slate-50";
+    rightButton.className = className;
+    rightButton.classList.add("right-4");
     focusDiv.appendChild(rightButton);
 
     container.appendChild(focusDiv);
@@ -159,6 +186,9 @@ export default function Slider() {
         animateCenter("next");
         animateLeft("next");
         animateRight("next");
+        console.log(activeNav);
+        activeNav.deactivate();
+        navButtons[center].activate();
     });
 
     leftButton.addEventListener("click", () => {
@@ -166,9 +196,24 @@ export default function Slider() {
         animateCenter("previous");
         animateLeft("previous");
         animateRight("previous");
+        console.log(activeNav);
+        activeNav.deactivate();
+        navButtons[center].activate();
     });
 
     parentContainer.appendChild(container);
+
+    const navDots = document.createElement("div");
+    navDots.classList =
+        "flex gap-3 absolute left-1/2 -translate-x-1/2 bottom-[7%] bg-black bg-opacity-70 px-2 py-2 rounded-full";
+    IMAGE_URLS.forEach((item, index) => {
+        const btnNav = NaVButton(index);
+        navDots.appendChild(btnNav.ButtonElement());
+        navButtons.push(btnNav);
+        if (index === 2) activeNav = btnNav;
+    });
+
+    parentContainer.appendChild(navDots);
 
     return parentContainer;
 }
